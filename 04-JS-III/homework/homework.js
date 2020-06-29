ed34b6e5d3b540022421a8d4e67b978165fa3eae
emiestevarena <emiliano.estevarena@gmail.com>
2020-06-29 12:51:20 -0300

    homework 3 to 5

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,16 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return array[0];
 }
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  let i=array.length-1;
+  return array[i];
 }
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  let i=array.length;
+  return i;
 }
 
 function incrementarPorUno(array) {
@@ -20,12 +25,18 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  parseInt(array);
+  let i = array.length;
+  for (let x=0; x<i; x++){array[x]++;}
+  return array;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -33,6 +44,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -41,39 +54,56 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  let y = palabras.join(' ');
+  return y;
 }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+ if(array.includes(elemento)){return true;}
+ else{return false;}
 }
 
 function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  let sum=0;
+  for(n of numeros){sum+=n;}
+  return sum;
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  parseInt(resultadosTest);
+  let i=resultadosTest.length;
+  let y=0;
+  for (let x=0; x<i; x++){y = y + resultadosTest[x];}
+  y = y/i;
+  return y;
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  return Math.max.apply(null, numeros);
 }
 
-function multiplicarArgumentos() {
+function multiplicarArgumentos(numeros) {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+  if(arguments.length<1){return 0;}
+  var total = 1;
+  for(let x=0;x<arguments.length;x++){total= total*arguments[x];}
+  return total;
 }
-
 // No modificar nada debajo de esta línea
 // --------------------------------
 
--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,23 +8,31 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-
+  function Usuario (opciones) {
+    this.usuario= opciones.usuario;
+    this.nombre= opciones.nombre;
+    this.email= opciones.email;
+    this.password= opciones.password;
+  }
+  Usuario.prototype.saludar = function () {
+    return "Hola, mi nombre es " + this.nombre;
+  }
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
-  // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
+  // El método debe llamarse "saludar" y debe devolver la string "Hello World!"ject.create({Usuario})
   // Tu código:
-
+  Constructor.prototype.saludar = function(){
+    return "Hello World!";
+  }
 }
 
 function agregarStringInvertida() {
-  // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
-  // El método debe llamarse "reverse"
-  // Ej: 'menem'.reverse() => menem
-  // 'toni'.reverse() => 'inot'
-  // Pista: Necesitarás usar "this" dentro de "reverse"
-
+  // Completed:
+  String.prototype.reverse = function(){
+  return this.split("").reverse().join("");}
 }
 
 // No modificar nada debajo de esta línea
